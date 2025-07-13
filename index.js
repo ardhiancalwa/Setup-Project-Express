11e94269a388c05739809d2e8f9c0ce3ad9d5b58
ardhiancalwa <ardhiancalwaofc@gmail.com>
2025-07-13 16:42:09 +0700

    feat(middleware): add implement logging, validation, authentication, and authorize

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 // 1. Mengimpor modul express
 const express = require("express");
-const db = require('./models');
+const db = require("./models");
+const morgan = require("morgan");
+require("dotenv").config();
 // 2. Membuat instance dari aplikasi express
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
-require("dotenv").config();
+const userRoutes = require("./src/routes/user.routes");
+const authRoutes = require("./src/routes/auth.routes");
+
+
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
 // 3. Mendefinisikan port untuk server
 // Menggunakan port dari environment variable jika ada, jika tidak, gunakan port 3000
 const port = process.env.PORT || 3000;
-async function testDbConnection() {
-	try {
-		await db.sequelize.authenticate();
-		console.log('Koneksi ke database berhasil terkoneksi.');
-	} catch (error) {
-		console.error('Tidak dapat terhubung ke database:', error);
-	}
-}
+// async function testDbConnection() {
+//   try {
+//     await db.sequelize.authenticate();
+//     console.log("Koneksi ke database berhasil terkoneksi.");
+//   } catch (error) {
+//     console.error("Tidak dapat terhubung ke database:", error);
+//   }
+// }
 
-testDbConnection();
+// testDbConnection();
+
 // 4. Mendefinisikan route sederhana untuk root URL ('/')
 app.get("/", (req, res) => {
   res.send("Hello World!");
--- models/user.js
+++ models/user.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -13,13 +11,21 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  User.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  User.init(
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      role: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: "user",
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
   return User;
 };
